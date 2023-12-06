de725ee060538cceab83c0a20b5c2afc80b49890
Kelven da Costa <devkelven@gmail.com>
2023-12-06 17:41:27 -0300

    projeto do modulo 5 criado
    projeto de agencia de viagens mvc java spring

---
+++ Entregas/Modulo-05/Pro_Agency/src/test/java/br/agency/ProAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
