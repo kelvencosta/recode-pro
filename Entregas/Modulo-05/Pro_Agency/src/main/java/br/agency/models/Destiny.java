12b05886573ace00dcbcfe483e7f292a72e67925
Kelven da Costa <devkelven@gmail.com>
2023-12-06 17:49:29 -0300

    model destiny criado
    Classe destino que cria uma tabela destinos no banco de dados mysql

---
+++ Entregas/Modulo-05/Pro_Agency/src/main/java/br/agency/models/Destiny.java
@@ -0,0 +1,72 @@
+package br.agency.models;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "destinations")
+public class Destiny {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false, unique = true)
+	private String imgUrl;
+	@Column(nullable = false)
+	private double price;
+
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	private String type;
+	
+	@OneToMany(mappedBy = "destiny")
+	private List<Reserve> reserve;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
